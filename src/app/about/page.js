1f955d4e7f4724c448f087de6d4dd4dc092d8b79
dharmik-tank-84 <dharmiktank84@gmail.com>
2024-07-12 19:49:18 +0530

    about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,16 @@
 const page = () => {
   return (
     <>
-      <p>about</p>
+      <div className="flex px-8">
+        <div className="border-2 flex-1 pt-[50px]">
+          <p className="text-xl">
+            I am a frontend web developer. I can provide clean code and pixel
+            perfect design. I also make website more & more interactive with web
+            animations.
+          </p>
+        </div>
+        <div className="flex-1"></div>
+      </div>
     </>
   );
 };
