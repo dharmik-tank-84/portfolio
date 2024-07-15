68a6b8fffb2e3dacd27393e2f7b145cddee4707f
dharmik-tank-84 <dharmiktank84@gmail.com>
2024-07-15 11:36:06 +0530

    navbar menu icon

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -1,10 +1,12 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 import style from "@/app/page.module.css";
 import { Oswald } from "next/font/google";
 import { Roboto } from "next/font/google";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const oswald = Oswald({
   weight: "700",
@@ -18,18 +20,32 @@ const roboto = Roboto({
   display: "swap",
 });
 
-export default function page() {
+export default function Page() {
+  const [menuOpen, setMenuOpen] = useState(false);
   const pathname = usePathname();
+
   return (
     <>
       <div className="flex items-center justify-between h-[80px] py-4 px-8">
         <p className={`text-4xl ${oswald.className} cursor-pointer`}>
           Dharmik Tank
         </p>
-        <ul className={`flex gap-5 ${roboto.className}`}>
+        <div className="md:hidden">
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-2xl focus:outline-none"
+          >
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </button>
+        </div>
+        <ul
+          className={`flex-col md:flex-row gap-5 absolute shadow-md md:shadow-none pb-4 md:pb-0 top-[100px] z-50 pl-8 md:pl-0 md:top-auto right-0 w-full md:w-auto bg-gray-100 md:relative md:flex  ${
+            roboto.className
+          } ${menuOpen ? "flex" : "hidden"}`}
+        >
           <li className="text-xl">
             <Link
-              className={` link ${pathname === "/" ? `${style.active}` : ""}`}
+              className={`link ${pathname === "/" ? `${style.active}` : ""}`}
               href="/"
             >
               Home
