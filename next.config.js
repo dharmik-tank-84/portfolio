40736ea086e4912b9dcd4df3ce4d81a0e29ca7c0
dharmik-tank-84 <dharmiktank84@gmail.com>
2024-07-25 18:34:25 +0530

    add more

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Configure Next.js for static export
-  // Add other configurations here if needed
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
