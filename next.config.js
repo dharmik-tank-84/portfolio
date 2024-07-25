2323b68311a6fad0d91dbe1756f18e265c7303e3
dharmik-tank-84 <dharmiktank84@gmail.com>
2024-07-25 18:23:48 +0530

    deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Configure Next.js for static export
+  // Add other configurations here if needed
+};
 
 module.exports = nextConfig;
